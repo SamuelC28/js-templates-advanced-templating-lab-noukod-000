0a236a5e24fb062d77a073bab05b3247fd015217
Samuel Cornet <samuel@noukod.com>
2019-02-18 16:26:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function init() {
+  handlebarsSetup()
   var formTemplate = document.getElementById("recipe-form-template").innerHTML
   var template = Handlebars.compile(formTemplate)
   document.getElementsByTagName("main")[0].innerHTML = template({'submitAction': 'createRecipe()'})
