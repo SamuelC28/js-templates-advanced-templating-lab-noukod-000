c91aeed2916ee8ac23857660ff68c84adc578d59
Samuel Cornet <samuel@noukod.com>
2019-02-18 16:01:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function initForm() {
+function init() {
   var formTemplate = document.getElementById("recipe-form-template").innerHTML
   var template = Handlebars.compile(formTemplate)
   document.getElementsByTagName("main")[0].innerHTML = template({'submitAction': 'createRecipe()'})
@@ -61,7 +61,7 @@ function handlebarsSetup() {
 function init() {
   //put any page initialization/handlebars initialization here
   handlebarsSetup()
-  initForm()
+  init()
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
